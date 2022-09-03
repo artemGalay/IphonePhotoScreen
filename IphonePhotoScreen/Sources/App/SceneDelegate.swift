20bfd2f02e8b7973b6ebabe832ae58e6c48a23d0
Artem Galay <iostema@mail.ru>
2022-09-03 11:58:04 +0300

    Create TabBarViewController and tabBar.items.

--- IphonePhotoScreen/Sources/App/SceneDelegate.swift
+++ IphonePhotoScreen/Sources/App/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let vc = ViewController()
+        let vc = MainTabBarController()
         let nc = UINavigationController(rootViewController: vc)
         window?.rootViewController = nc
         window?.makeKeyAndVisible()
---
+++ IphonePhotoScreen/Sources/Controllers/MainTabBarController.swift
@@ -0,0 +1,49 @@
+//
+//  MainTabBarController.swift
+//  IphonePhotoScreen
+//
+//  Created by Артем Галай on 3.09.22.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTabBar()
+        setupHierarchy()
+    }
+
+    private func  setupTabBar() {
+        tabBar.backgroundColor = .tertiarySystemFill
+        tabBar.tintColor = .systemBlue
+
+
+    }
+    private func setupHierarchy() {
+
+        let mediaLibraryVC = MediaLibraryViewController()
+        let forYouVC = ForYouViewController()
+        let albumcVC = AlbumsViewController()
+        let searchVC = SearchViewController()
+
+        setViewControllers([mediaLibraryVC, forYouVC, albumcVC, searchVC], animated: true)
+
+        guard let items = tabBar.items else { return }
+
+        items[0].title = "Медиатека"
+        items[1].title = "Для Вас"
+        items[2].title = "Альбомы"
+        items[3].title = "Поиск"
+
+        items[0].image = UIImage(systemName: "photo.fill.on.rectangle.fill")
+        items[1].image = UIImage(systemName: "heart.text.square.fill")
+        items[2].image = UIImage(systemName: "square.stack.fill")
+        items[3].image = UIImage(systemName: "magnifyingglass")
+
+        UITabBarItem.appearance().setTitleTextAttributes([.font: UIFont(name: "Arial", size: 13) as Any],
+                                                         for: .normal)
+    }
+}
