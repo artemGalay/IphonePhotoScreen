1b748c8722f63fb9a796c88156683b429f4474a7
Artem Galay <iostema@mail.ru>
2022-09-06 10:09:12 +0300

    Add func layoutSectionHeader.

--- IphonePhotoScreen/Sources/Items/TypeMediaFilesCell.swift
+++ IphonePhotoScreen/Sources/Items/TypeMediaFilesCell.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class TypeMediaFilesCell: UICollectionViewCell {
-
+    
     static let identifier = "TypeMediaFilesCell"
-
+    
     var contents: AlbumsModel? {
         didSet {
             image.image = UIImage(systemName: contents?.image ?? "")
@@ -18,7 +18,7 @@ class TypeMediaFilesCell: UICollectionViewCell {
             numberLabel.text = contents?.number
         }
     }
-
+    
     lazy var image: UIImageView = {
         let image = UIImageView()
         image.clipsToBounds = true
@@ -26,7 +26,7 @@ class TypeMediaFilesCell: UICollectionViewCell {
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
-
+    
     lazy var descriptionLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 22)
@@ -34,7 +34,7 @@ class TypeMediaFilesCell: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     lazy var numberLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 20)
@@ -42,7 +42,7 @@ class TypeMediaFilesCell: UICollectionViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     lazy var imageChevron: UIImageView = {
         let image = UIImageView()
         image.clipsToBounds = true
@@ -52,7 +52,7 @@ class TypeMediaFilesCell: UICollectionViewCell {
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
-
+    
     lazy var mainStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .horizontal
@@ -62,17 +62,17 @@ class TypeMediaFilesCell: UICollectionViewCell {
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupHierarchy()
         setupLayout()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     private func setupHierarchy() {
         contentView.addSubview(mainStack)
         mainStack.addArrangedSubview(image)
@@ -80,24 +80,24 @@ class TypeMediaFilesCell: UICollectionViewCell {
         mainStack.addArrangedSubview(numberLabel)
         mainStack.addArrangedSubview(imageChevron)
     }
-
+    
     private func setupLayout() {
         NSLayoutConstraint.activate([
             mainStack.topAnchor.constraint(equalTo: contentView.topAnchor),
             mainStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             mainStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             mainStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-
+            
             mainStack.heightAnchor.constraint(equalToConstant: 200),
             mainStack.widthAnchor.constraint(equalToConstant: 400),
-
+            
             image.widthAnchor.constraint(equalToConstant: 35),
             image.heightAnchor.constraint(equalToConstant: 35),
-
+            
             descriptionLabel.centerYAnchor.constraint(equalTo: image.centerYAnchor),
-
+            
             numberLabel.centerYAnchor.constraint(equalTo: image.centerYAnchor),
-
+            
             imageChevron.centerYAnchor.constraint(equalTo: image.centerYAnchor),
         ])
     }
